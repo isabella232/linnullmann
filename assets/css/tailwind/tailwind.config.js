43ff0b7928bf32f665307383831fcc63b1cb5ff2
budparr <budparr@gmail.com>
2019-01-13 12:31:43 -0500

    [wip] Add reviews

--- assets/css/tailwind/tailwind.config.js
+++ assets/css/tailwind/tailwind.config.js
@@ -152,7 +152,7 @@ module.exports = {
     '2xl': '1.5rem',    // 24px
     '3xl': '1.875rem',  // 30px
     '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    '5xl': '3.5rem',      // 56px, adjusted from 3rem
   },
 
   fontWeights: {
@@ -270,6 +270,7 @@ module.exports = {
 
   minHeight: {
     '0': '0',
+    '100': '25rem',
     'full': '100%',
     'screen': '100vh',
   },
